9c2a4b298aeb9c0e67fe35340cb4ecc3c69f50b4
iOsnaaente <bruno.bielsam.1205@hotmail.com>
2019-05-08 20:54:33 -0300

    criação da classe que interpreta as fases e reestruturação da arvore do código por falta de conhecimento na parte de organização

--- __init__.py
+++
@@ -1,5 +0,0 @@
-#! usr/bin/dev python
-
-from code import *
-from main import *
-
--- main.py
+++ main.py
@@ -1,5 +1,51 @@
-#! usr/bin/dev python	
+#! usr/bin/dev python
 
-from 'Games-Puzzles' import main 
+from stage_interpreter import Stages	#Le as fases
+from tanks_ import PlayerTank 			#Responsável pelos tanques do player
+from imagens import *					#imagens do jogo
+
+import pygame
+import random
+
+
+screen_Dimension=[32*20,32*20]
+
+pygame.init()
+
+screen = pygame.display.set_mode(screen_Dimension)
+
+pygame.display.set_caption("My_Poor_NES_Batlle_City")
+
+clock = pygame.time.Clock()
+
+
+Fase_1 = Stages(screen)
+Fase_1.readStage(1)
+
+
+Tank = PlayerTank(blueTank, [64,64], screen)
+
+while True:
+
+	screen.fill([0,0,0])
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				pygame.quit()
+
+			Tank.move(event)
+
+
+
+	Fase_1.plotStage()
+	Tank.plot()
+	pygame.display.update()
+
+
+	clock.tick(60)
+
 
 
