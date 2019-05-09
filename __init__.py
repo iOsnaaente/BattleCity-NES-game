206ef86541534cbe6d5f0f8f7ecfe2e05667bf20
iOsnaaente <bruno.bielsam.1205@hotmail.com>
2019-05-09 12:00:40 -0300

    ENFIM MODULARIZADO

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 #! usr/bin/dev python
+
+from images import *
+from stages import *
+from images import *
+from views import *
+from code import *
--- images/imagens.py
+++ images/imagens.py
@@ -32,7 +32,36 @@ star   = pygame.image.load(os.path.join('images',"star.png"))
 #bomb
 
 
+""" 
+# TANQUES INIMIGOS
+redTank1 = pygame.image.load("redTank1.png")
+redTank2 = pygame.image.load("redTank2.png")
+redTank3 = pygame.image.load("redTank3.png")
+redTank4 = pygame.image.load("redTank4.png")
 
+# TANQUES JOGADOR
+blueTank = pygame.image.load('blueTank.png')
+greenTank = pygame.image.load('greenTank.png')
+
+# TERRENOS
+bushes = pygame.image.load("bushes.png")
+bushes   = pygame.transform.scale(bushes, (32,32))
+
+bricks = pygame.image.load("brick.png")
+pool   = pygame.image.load("pool.png")
+
+iron   = pygame.image.load("iron.png")
+iron   = pygame.transform.scale(iron, (32,32))
+
+ice    = pygame.image.load("ice.png")
+
+# ITENS
+shieldTanque1 = pygame.image.load("shieldTanque1.png")
+shieldTanque2 = pygame.image.load("shieldTanque2.png")
+
+shovel = pygame.image.load("shovel.png")
+star   = pygame.image.load("star.png")
+"""
 
 
 
